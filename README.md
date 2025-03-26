29cb8158acaf50e3ea8a07d878b6301ef993cc68
ice_BEAR <spoggie.hs@gmail.com>
2025-03-26 09:03:53 +0200

    Project base code

---
+++ README.md
@@ -0,0 +1,2 @@
+Our ADP372S capstone project code.
+A booking system whereby students who study ICT:Appdev can book tutors for certain subjects.
---
+++ src/main/java/za/ac/cput/domain/Tutor.java
@@ -0,0 +1,10 @@
+/*
+Tutor.java
+Tutor model class
+Author: Henzley Spogter (230013309)
+Date: 26 March 2025
+ */
+package za.ac.cput.domain;
+
+public class Tutor {
+}
