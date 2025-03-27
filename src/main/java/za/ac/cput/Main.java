d918272d611316eea47b0ae09fa57c54885d3f5f
ice_BEAR <spoggie.hs@gmail.com>
2025-03-27 14:19:04 +0200

    Test 'Booking' domain object creation

--- src/main/java/za/ac/cput/Main.java
+++ src/main/java/za/ac/cput/Main.java
@@ -1,5 +1,17 @@
 package za.ac.cput;
 
-public class Main {
+import za.ac.cput.domain.Booking;
 
+public class Main {
+    public static void main(String[] args) {
+
+        //Test booking object creation
+//        Booking booking1 = new Booking.BookingBuilder("123456789",
+//                "23 April 2025",
+//                "12h30",
+//                "23072000",
+//                "R500",
+//                "Confirmed").build();
+//        System.out.println(booking1);
+    }
 }
